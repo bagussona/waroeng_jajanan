5b86c9ec0dac0024c2e75ee811f9bf760a4109f1
bagussona <bagussona87@gmail.com>
2021-07-29 21:32:05 +0700

    Rebuild System v8

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/product';
 
     /**
      * Define your route model bindings, pattern filters, etc.
