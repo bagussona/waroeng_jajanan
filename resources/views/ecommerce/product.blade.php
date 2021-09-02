78226326bbe70cf33aca5f8d0177063731e44d99
bagussona <bagussona87@gmail.com>
2021-09-02 17:17:00 +0700

    Rebuild System v42

--- resources/views/ecommerce/product.blade.php
+++ resources/views/ecommerce/product.blade.php
@@ -2,6 +2,60 @@
 
 @section('title')
     <title>{{ env('APP_NAME') }}</title>
+
+    <style>
+
+        .search__container {
+            width: 500px;
+        }
+
+        .search__title {
+            font-size: 22px;
+            font-weight: 900;
+            text-align: center;
+            color: #ff8b88;
+        }
+
+        .search__input {
+            width: 100%;
+            padding: 12px 24px;
+
+            background-color: transparent;
+            transition: transform 250ms ease-in-out;
+            font-size: 14px;
+            line-height: 18px;
+
+            color: #575756;
+            background-color: transparent;
+
+            background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
+            background-repeat: no-repeat;
+            background-size: 18px 18px;
+            background-position: 95% center;
+            border-radius: 50px;
+            border: 1px solid #575756;
+            transition: all 250ms ease-in-out;
+            backface-visibility: hidden;
+            transform-style: preserve-3d;
+        }
+
+        .search__input::placeholder {
+            color: rgba(87, 87, 86, 0.8);
+            text-transform: uppercase;
+            letter-spacing: 1.5px;
+        }
+
+        .search__input:hover,
+        .search__input:focus {
+            padding: 12px 0;
+            outline: 0;
+            border: 1px solid transparent;
+            border-bottom: 1px solid #575756;
+            border-radius: 0;
+            background-position: 100% center;
+        }
+
+    </style>
 @endsection
 
 @section('content')
@@ -28,16 +82,9 @@
                 <div class="col-lg-9">
                     <div class="product_top_bar">
                         <div class="left_dorp">
-                            <select class="sorting">
-                                <option value="1">Default sorting</option>
-                                <option value="2">Default sorting 01</option>
-                                <option value="4">Default sorting 02</option>
-                            </select>
-                            <select class="show">
-                                <option value="1">Show 12</option>
-                                <option value="2">Show 14</option>
-                                <option value="4">Show 16</option>
-                            </select>
+                            <div class="search__container">
+                                <input class="search__input" type="text" placeholder="Search">
+                            </div>
                         </div>
                         <div class="right_page ml-auto">
                             {{ $products->links() }}
