c0cc1d6da15da87dcfdb4e8b8170c1b088496d1e
bagussona <bagussona87@gmail.com>
2021-09-02 16:46:14 +0700

    Rebuild System v40.5

--- app/Http/Controllers/Web/ReportsController.php
+++ app/Http/Controllers/Web/ReportsController.php
@@ -46,7 +46,8 @@ class ReportsController extends Controller
 
         $current_date = date('Y-m-d');
 
-        $order_all = DB::table('order_details')->select('name as products', 'price', DB::raw("SUM(qty) as terjual", "SUM(subtotal) as subtotal"))->groupBy('products', 'price')->havingRaw('SUM(qty) * price > 1', [2500])->where('created_at', 'LIKE', '%' . "$current_date" . '%')->get();
+        $order_all = DB::table('order_details')->select('name as products', 'price', DB::raw('SUM(qty) as terjual, SUM(subtotal) as subtotal'))->groupBy('products', 'price')->havingRaw('SUM(qty) * price > 1')->where('created_at', 'LIKE', '%' . $current_date . '%')->get();
+        // $order_all = DB::table('order_details')->select('name as products', 'price', DB::raw("SUM(qty) as terjual", "SUM(subtotal) as subtotal"))->groupBy('products', 'price')->havingRaw('SUM(qty) * price > 1', [2500])->where('created_at', 'LIKE', '%' . "$current_date" . '%')->get();
         // dd($order_all);
 
         $order_details = OrderDetail::where('created_at', 'LIKE', '%' . $current_date . '%')->get();
