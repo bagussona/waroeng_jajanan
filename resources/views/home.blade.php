ccda24a868fccf4d59a9b39239649d4ba2c2553d
bagussona <bagussona87@gmail.com>
2021-09-09 14:38:19 +0700

    Home Dashboard updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,5 +73,292 @@
             </div>
         </div>
     </div>
+    <div class="container-fluid">
+        <div class="animated fadeIn">
+            <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Laporan Keuangan Pekanan
+                    </div>
+                        <div class="card-body">
+
+                        <div class="col-md-12 mb-4">
+                            <ul class="nav nav-tabs" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#pekan1" role="tab" aria-controls="pekan1">
+                                <i class="icon-calculator"></i> Pekan 1
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#pekan2" role="tab" aria-controls="pekan2">
+                                <i class="icon-calculator"></i> Pekan 2
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#pekan3" role="tab" aria-controls="pekan3">
+                                <i class="icon-calculator"></i> Pekan 3
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#pekan4" role="tab" aria-controls="pekan4">
+                                <i class="icon-calculator"></i> Pekan 4
+                                </a>
+                            </li>
+                            </ul>
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="pekan1" role="tabpanel">
+
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-2">
+                                                <div class="callout callout-info">
+                                                    <small class="text-muted">Total Penjualan</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 1142000</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-danger">
+                                                    <small class="text-muted">Keuntungan Didapat</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 168000 </strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-primary">
+                                                    <small class="text-muted">Sisa Barang (Rupiah)</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 2000</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Hutang Tersisa</small>
+                                                    <br>
+                                                    <strong class="h4">0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Modal</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 974000</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Pelanggan Daftar</small>
+                                                    <br>
+                                                    <strong class="h4">31</strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="tab-pane" id="pekan2" role="tabpanel">
+
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-2">
+                                                <div class="callout callout-info">
+                                                    <small class="text-muted">Total Penjualan</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-danger">
+                                                    <small class="text-muted">Keuntungan Didapat</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0 </strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-primary">
+                                                    <small class="text-muted">Sisa Barang (Rupiah)</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Hutang Tersisa</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Modal</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Pelanggan Daftar</small>
+                                                    <br>
+                                                    <strong class="h4">0</strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="tab-pane" id="pekan3" role="tabpanel">
+
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-2">
+                                                <div class="callout callout-info">
+                                                    <small class="text-muted">Total Penjualan</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-danger">
+                                                    <small class="text-muted">Keuntungan Didapat</small>
+                                                    <br>
+                                                    <strong class="h4">Rp.  0 </strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-primary">
+                                                    <small class="text-muted">Sisa Barang (Rupiah)</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Hutang Tersisa</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Modal</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Pelanggan Daftar</small>
+                                                    <br>
+                                                    <strong class="h4">0</strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="tab-pane" id="pekan4" role="tabpanel">
+
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-2">
+                                                <div class="callout callout-info">
+                                                    <small class="text-muted">Total Penjualan</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-danger">
+                                                    <small class="text-muted">Keuntungan Didapat</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0 </strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-primary">
+                                                    <small class="text-muted">Sisa Barang (Rupiah)</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Hutang Tersisa</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Modal</small>
+                                                    <br>
+                                                    <strong class="h4">Rp. 0</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="callout callout-success">
+                                                    <small class="text-muted">Pelanggan Daftar</small>
+                                                    <br>
+                                                    <strong class="h4">0</strong>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 style="margin-bottom: 0;" class="card-title">Laporan Pembagian Hasil</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="callout callout-success">
+                                        <small class="text-muted">Total Keuntungan</small>
+                                        <br>
+                                        <strong class="h4">Rp. 0</strong>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="callout callout-info">
+                                        <small class="text-muted">Pondok</small>
+                                        <br>
+                                        <strong class="h4">50 %</strong>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="callout callout-danger">
+                                        <small class="text-muted">Investor</small>
+                                        <br>
+                                        <strong class="h4">Rp. 0 x5</strong>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="callout callout-primary">
+                                        <small class="text-muted">Gaji Karyawan</small>
+                                        <br>
+                                        <strong class="h4">Rp. 0 x2</strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 @endsection
