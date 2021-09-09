d32cbf44f1227e6c1edda0f8b25e21f37da83a41
bagussona <bagussona87@gmail.com>
2021-09-09 15:03:24 +0700

    Home Dashboard updated icon

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -9,12 +9,12 @@
         <li class="nav-title">MANAJEMEN USER</li>
         <li class="nav-item nav-dropdown">
             <a class="nav-link nav-dropdown-toggle" href="#">
-                <i class="nav-icon icon-settings"></i> Pengguna
+                <i class="nav-icon icon-user"></i> Pengguna
             </a>
             <ul class="nav-dropdown-items">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('members.index') }}">
-                        <i class="nav-icon icon-puzzle"></i> Semua Pengguna
+                        <i class="nav-icon icon-people"></i> Semua Pengguna
                     </a>
                 </li>
             </ul>
@@ -22,17 +22,17 @@
         <li class="nav-title">MANAJEMEN PRODUK</li>
         <li class="nav-item">
             <a class="nav-link" href="{{ route('category.index') }}">
-                <i class="nav-icon icon-drop"></i> Kategori
+                <i class="nav-icon icon-tag"></i> Kategori
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="{{ route('supplier.index') }}">
-                <i class="nav-icon icon-drop"></i> Supplier
+                <i class="nav-icon icon-pie-chart"></i> Supplier
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="{{ route('orderan.index') }}">
-                <i class="nav-icon icon-drop"></i> Orderan
+                <i class="nav-icon icon-basket-loaded"></i> Orderan
                 <span style="margin-left: 15px;" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                     {{ $ob }}
                 </span>
@@ -40,56 +40,51 @@
         </li>
         <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-settings"></i> Product
+            <i class="nav-icon icon-organization"></i> Product
         </a>
         <ul class="nav-dropdown-items">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('products.index') }}">
-                    <i class="nav-icon icon-puzzle"></i> Product Warehouse
+                    <i class="nav-icon icon-organization"></i> Product Warehouse
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('products.display') }}">
-                    <i class="nav-icon icon-puzzle"></i> Product Display
+                    <i class="nav-icon icon-screen-desktop"></i> Product Display
                 </a>
             </li>
         </ul>
         </li>
         <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-settings"></i> SPB
+            <i class="nav-icon fas fa-truck"></i> SPB
         </a>
         <ul class="nav-dropdown-items">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('datastore.index') }}">
-                    <i class="nav-icon icon-puzzle"></i> BBK
+                    <i class="nav-icon fas fa-box-open"></i> BBK
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('datastore.bbm') }}">
-                    <i class="nav-icon icon-puzzle"></i> BBM
+                    <i class="nav-icon fas fa-box"></i> BBM
                 </a>
             </li>
         </ul>
         </li>
         <li class="nav-item nav-dropdown">
         <a class="nav-link nav-dropdown-toggle" href="#">
-            <i class="nav-icon icon-settings"></i> Laporan
+            <i class="nav-icon icon-note"></i> Laporan
         </a>
         <ul class="nav-dropdown-items">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('reports.daily') }}">
-                    <i class="nav-icon icon-puzzle"></i> Akhiri Shift
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">
-                    <i class="nav-icon icon-puzzle"></i> Laporan Bulanan
+                    <i class="nav-icon icon-notebook"></i> Akhiri Shift
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('reports.inquiry') }}">
-                    <i class="nav-icon icon-puzzle"></i> Inquiry Transaksi
+                    <i class="nav-icon icon-calculator"></i> Inquiry Transaksi
                 </a>
             </li>
         </ul>
@@ -101,7 +96,7 @@
         <ul class="nav-dropdown-items">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('front.index') }}">
-                        <i class="nav-icon icon-puzzle"></i> Toko
+                        <i class="nav-icon icon-bag"></i> Toko
                     </a>
                 </li>
             </ul>
